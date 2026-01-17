82ce58fdff71276afd6841c1ac8e3c57f197607c
Məhəmməd <mmustafa@std.beu.edu.az>
2026-01-17 21:04:38 +0400

    Update Cyber.py

--- Cyber.py
+++ Cyber.py
@@ -1,16 +1,15 @@
 def check_password(password):
     if len(password) < 6:
-        return "Zəif parol ❌"
+        return "Weak password"
 
     has_upper = any(c.isupper() for c in password)
     has_lower = any(c.islower() for c in password)
     has_digit = any(c.isdigit() for c in password)
 
     if has_upper and has_lower and has_digit:
-        return "Güclü parol ✅"
+        return "Secure password"
     else:
-        return "Orta parol ⚠️"
+        return "Medium password"
 
-
-pwd = input("Parolu daxil et: ")
-print(check_password(pwd))
+password=input("Enter the password: ")
+print(check_password(password))
